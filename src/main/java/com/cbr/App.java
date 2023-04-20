f1598631d222e0f1b492fc16ed81941c1cc24027
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-04-20 22:09:35 +0700

    feat: added javafx dependency

---
+++ src/main/java/com/cbr/App.java
@@ -0,0 +1,31 @@
+package com;
+
+import javafx.application.Application;
+import javafx.scene.control.Label;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+
+
+public class App extends Application{
+    Label label;
+    public static void main (String[] args) {
+        System.out.println("hello");
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage stage) throws Exception {
+        stage.setTitle("Business Management Application");
+
+        StackPane layout = new StackPane();
+        label = new Label("Hello, World!");
+        layout.getChildren().add(label);
+
+        Scene scene = new Scene(layout, 300, 250);
+
+        stage.setScene(scene);
+        stage.show();
+    }
+}
+
