12ae848353c1e5d00031c522ec1a24c5b42e95fc
chaerla <chelgchen@gmail.com>
2023-04-28 00:43:28 +0800

    commented assets

--- src/main/java/com/cbr/App.java
+++ src/main/java/com/cbr/App.java
@@ -1,14 +1,19 @@
 package com.cbr;
 
+import com.cbr.datastore.DataStore;
 import com.cbr.view.MainView;
+import com.cbr.view.theme.Theme;
 import javafx.application.Application;
 import javafx.application.Platform;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
 import javafx.scene.Scene;
+import lombok.Getter;
 
 
 public class App extends Application{
+    @Getter
+    private static DataStore dataStore = new DataStore("JSON", "assets/data/json");
     public static void main (String[] args) {
         System.out.println("Starting App...");
         launch(args);
@@ -17,12 +22,12 @@ public class App extends Application{
     @Override
     public void start(Stage stage) throws Exception {
         stage.setTitle("Business Management Application");
-        Image appIcon = new Image("/icons/capybucks.jpg");
+//        Image appIcon = new Image("/assets/icons/capybucks.jpg");
         // chore: update icons
-        stage.getIcons().add(appIcon);
+//        stage.getIcons().add(appIcon);
 
         MainView mainView = new MainView();
-        Scene scene = new Scene(mainView, 1280, 720);
+        Scene scene = new Scene(mainView, Theme.getScreenWidth(), Theme.getScreenHeight());
 
         stage.setScene(scene);
         stage.setResizable(false);
