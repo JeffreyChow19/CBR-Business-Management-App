7adcae6e7427e09e2cf35f3e1891aaf3d1367a74
chaerla <chelgchen@gmail.com>
2023-04-27 16:03:11 +0800

    feat: product constructor

--- src/main/java/com/cbr/models/Product.java
+++ src/main/java/com/cbr/models/Product.java
@@ -2,15 +2,14 @@ package com.cbr.models;
 
 import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 import java.io.Serializable;
-
-@AllArgsConstructor
+@NoArgsConstructor
 @Getter
 @Setter
-public class Product implements Serializable {
-    private Integer id;
+public class Product extends Identifiable implements Serializable {
     private String productName;
     private Double buyPrice;
     private Double sellPrice;
@@ -18,4 +17,17 @@ public class Product implements Serializable {
     private Integer stock;
     private String category;
     private Boolean status;
+    private static Integer productCount = 0;
+
+    public Product(String productName, Double buyPrice, Double sellPrice, Integer imagePath, Integer stock, String category, Boolean status){
+        this.productName = productName;
+        this.buyPrice = buyPrice;
+        this.sellPrice = sellPrice;
+        this.imagePath = imagePath;
+        this.stock = stock;
+        this.category = category;
+        this.status = status;
+        Product.productCount+=1;
+        this.id = "P-" + Product.productCount.toString();
+    }
 }
