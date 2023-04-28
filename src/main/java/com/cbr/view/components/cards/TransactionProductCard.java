49411b84b11f40f633b3344e35b617befad9a6a7
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-04-28 23:33:39 +0700

    feat: transaction product card

--- src/main/java/com/cbr/view/components/cards/TransactionProductCard.java
+++ src/main/java/com/cbr/view/components/cards/TransactionProductCard.java
@@ -1,38 +1,77 @@
 package com.cbr.view.components.cards;
 
 import com.cbr.models.Product;
+import com.cbr.view.components.buttons.PlusButton;
+import com.cbr.view.components.labels.ToolTipLabel;
 import com.cbr.view.theme.Theme;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
+import javafx.scene.control.Button;
 import javafx.scene.control.Label;
-import javafx.scene.layout.VBox;
+import javafx.scene.control.Tooltip;
+import javafx.scene.layout.*;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.*;
+import javafx.util.Duration;
 
+import java.io.File;
 
-public class TransactionProductCard extends VBox {
+public class TransactionProductCard extends StackPane {
 
     public TransactionProductCard(Product product){
-        this.setWidth(250);
-        this.setHeight(100);
+        double width = 0.1 * Theme.getScreenWidth();
+        double height = 0.2 * Theme.getScreenHeight();
 
-        System.out.println(this.getHeight());
-        System.out.println(this.getWidth());
+        // Set Card Size
+        this.setMinSize(width, height);
+        this.setPrefSize(width, height);
+        this.setMaxSize(width, height);
 
-//        Image image = new Image("file:assets/images/empty.jpg");
-//        ImageView imageView = new ImageView();
-//        imageView.setImage(image);
-//        imageView.setFitWidth(this.getWidth());
-//        imageView.setFitHeight(0.85 * this.getHeight());
-//
-//        this.getChildren().add(imageView);
+        // Product Image
+        // Image image = new Image(product.getImagePath());
+        Image image = new Image("file:assets/images/empty.jpg");
+        ImageView imageView = new ImageView(image);
+        imageView.setFitWidth(width);
+        imageView.setFitHeight(0.8 * height);
+        Rectangle clip = new Rectangle(imageView.getFitWidth(), imageView.getFitHeight());
+        clip.setArcWidth(20);
+        clip.setArcHeight(20);
+        imageView.setClip(clip);
+        this.getChildren().add(imageView);
 
-        Label label = new Label("hello");
-        this.getChildren().add(label);
+        // Label and Add Button HBox
+        HBox label_button = new HBox();
+        label_button.setMinSize(width, 0.25 * height);
+        label_button.setPrefSize(width, 0.25 * height);
+        label_button.setMaxSize(width, 0.25 * height);
+        label_button.setStyle("-fx-background-color:" + Theme.getSecondaryBase() + ";  -fx-background-radius: 0 0 10 10;");
+        label_button.setAlignment(Pos.CENTER);
+        StackPane.setAlignment(label_button, Pos.BOTTOM_CENTER);
 
-        this.setPadding(new Insets(20));
-        this.setAlignment(Pos.CENTER);
+        // Product Label
+        Label productLabel = new ToolTipLabel(product.getProductName());
+        productLabel.setFont(Theme.getHeading2Font());
+        productLabel.setTextFill(Color.WHITE);
 
-        this.setStyle("-fx-background-color:" + Theme.getPrimaryBase() + ";  -fx-background-radius: 10;");
+        // Add Region
+        Region region = new Region();
+        label_button.setHgrow(region, Priority.ALWAYS);
+
+        // Add Product Button
+        Button addButton = new PlusButton(0.07 * height);
+        addButton.setOnMouseClicked(event -> {
+            System.out.println("hello world");
+        });
+
+        label_button.setPadding(new Insets(10));
+        label_button.getChildren().addAll(productLabel, region, addButton);
+
+        this.getChildren().add(label_button);
+
+        this.setAlignment(Pos.TOP_CENTER);
+
+        this.setStyle("-fx-background-color:" + Theme.getSecondaryBase() + ";  -fx-background-radius: 10;");
     }
 }
