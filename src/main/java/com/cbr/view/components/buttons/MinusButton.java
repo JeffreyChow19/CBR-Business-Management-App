821606e14ef32ed792b7a7ed3c89eeb15d8a0e7d
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-05-01 01:51:46 +0700

    feat: Minus Button

---
+++ src/main/java/com/cbr/view/components/buttons/MinusButton.java
@@ -0,0 +1,12 @@
+package com.cbr.view.components.buttons;
+
+import com.cbr.view.theme.Theme;
+
+
+public class MinusButton extends CircleImageButton {
+    public MinusButton(double radius) {
+        super(radius, "file:assets/icons/plus.png", Theme.getPrimaryLight());
+    }
+}
+
+
