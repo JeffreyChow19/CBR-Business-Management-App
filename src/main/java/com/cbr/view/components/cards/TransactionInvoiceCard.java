1313b1fc9d406f4e814072a29add07bdc088c44c
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-05-01 01:52:09 +0700

    feat: init TransactionInvoiceCard

---
+++ src/main/java/com/cbr/view/components/cards/TransactionInvoiceCard.java
@@ -0,0 +1,9 @@
+package com.cbr.view.components.cards;
+
+import javafx.scene.layout.HBox;
+
+public class TransactionInvoiceCard extends HBox {
+    public TransactionInvoiceCard(String name, double price) {
+
+    }
+}
