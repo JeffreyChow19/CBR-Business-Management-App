271e1c164fc9b0a445f43af0bd0864d4ac6b6158
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-05-01 01:52:33 +0700

    feat: Number Spinner

---
+++ src/main/java/com/cbr/view/components/spinner/NumberSpinner.java
@@ -0,0 +1,59 @@
+package com.cbr.view.components.spinner;
+
+import com.cbr.view.components.buttons.MinusButton;
+import com.cbr.view.components.buttons.PlusButton;
+import com.cbr.view.theme.Theme;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.layout.HBox;
+
+public class NumberSpinner extends HBox {
+    private int value;
+    private Label valueLabel;
+
+    public NumberSpinner(int initialValue) {
+        this.value = initialValue;
+
+        Button decrementButton = new MinusButton(10);
+        decrementButton.setOnAction(event -> decrementValue());
+
+        valueLabel = new Label(Integer.toString(value));
+        valueLabel.setFont(Theme.getBodyMediumFont());
+        valueLabel.setStyle("-fx-text-fill: white");
+        double minWidth = com.sun.javafx.tk.Toolkit.getToolkit().getFontLoader().computeStringWidth("00", valueLabel.getFont());
+        valueLabel.setMinWidth(minWidth);
+        valueLabel.setAlignment(Pos.CENTER);
+
+        Button incrementButton = new PlusButton(10);
+        incrementButton.setOnAction(event -> incrementValue());
+
+        this.getChildren().addAll(decrementButton, valueLabel, incrementButton);
+        this.setSpacing(10);
+        this.setAlignment(Pos.CENTER);
+        this.setPadding(new Insets(5));
+    }
+
+    public int getValue() {
+        return value;
+    }
+
+    private void decrementValue() {
+        if (value > 1) {
+            value--;
+            updateValueLabel();
+        } else {
+            // Remove this record
+        }
+    }
+
+    private void incrementValue() {
+        value++;
+        updateValueLabel();
+    }
+
+    private void updateValueLabel() {
+        valueLabel.setText(Integer.toString(value));
+    }
+}
