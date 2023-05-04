32c8e2a4c9dd10697470ddc7b89a6d3fb96f8618
chaerla <chelgchen@gmail.com>
2023-05-04 22:58:52 +0800

    add currency plugin

--- Plugin-System-1/src/main/java/com/cbr/Currency.java
+++ Plugin-System-1/src/main/java/com/cbr/Currency.java
@@ -1,6 +1,54 @@
 package com.cbr;
 
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.util.DefaultIndenter;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class Currency {
+    private String symbol;
+    private String name;
     private Integer value;
-    private Float exchangeRate; // relative to USD
+    private Double exchangeRate; // relative to USD
+
+    public static void main(String[] args) {
+        ObjectMapper mapper = new ObjectMapper();
+        mapper.registerModule(new JavaTimeModule());
+        DefaultPrettyPrinter prettyPrinter = new DefaultPrettyPrinter();
+        prettyPrinter.indentArraysWith(DefaultIndenter.SYSTEM_LINEFEED_INSTANCE);
+        prettyPrinter.indentObjectsWith(DefaultIndenter.SYSTEM_LINEFEED_INSTANCE);
+        mapper.setDefaultPrettyPrinter(prettyPrinter);
+        ObjectWriter writer = mapper.writer(prettyPrinter);
+
+        Currency IDR = new Currency("IDR", "Indonesian Rupiah", 1, 1.0);
+        Currency USD = new Currency ("USD", "US Dollar", 1, 14000.0);
+        Currency AUD = new Currency ("AUD", "Australian Dollar", 1, 10000.0);
+        Currency KRW = new Currency ("KRW", "Korean Won", 1, 11.0);
+
+        List<Currency> curs = Arrays.asList(IDR, USD, AUD, KRW);
+
+        try{
+            String jsonDataString = writer.writeValueAsString(curs);
+            Files.write(Paths.get("D:/lessons/SEM 4/OOP/TUBES/backup/CBR-Business-Management-App/Plugin-System-1/src/main/resources", "currency.json"), jsonDataString.getBytes());
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+
+    }
 }
--- Plugin-System-1/src/main/java/com/cbr/CurrencyPlugin.java
+++ Plugin-System-1/src/main/java/com/cbr/CurrencyPlugin.java
@@ -4,16 +4,55 @@ import com.cbr.plugin.Plugin;
 import com.cbr.view.MainView;
 import com.cbr.view.components.dropdown.Dropdown;
 import com.cbr.view.theme.Theme;
-import com.mifmif.common.regex.Main;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.core.type.TypeReference;
+import com.fasterxml.jackson.core.util.DefaultIndenter;
+import com.fasterxml.jackson.core.util.DefaultPrettyPrinter;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.ObjectWriter;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import javafx.scene.control.Label;
 import javafx.scene.paint.Color;
 
+import java.io.IOException;
+import java.net.URISyntaxException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.Arrays;
+import java.util.List;
+import java.util.stream.Collectors;
+
 public class CurrencyPlugin implements Plugin {
+
+    public CurrencyPlugin(){
+
+    }
     public void load(){
         Label currencyLabel = new Label("Currency");
         currencyLabel.setFont(Theme.getBodyFont());
         currencyLabel.setTextFill(Color.WHITE);
-//        Dropdown dropdown = new Dropdown();
-//        MainView.getInstance().getSettingsPage().getFormContainer().getChildren().addAll(currencyLabel, dropdown);
+        try {
+            ObjectMapper mapper = new ObjectMapper();
+            System.out.println((Paths.get(getClass().getResource("currency.json").toURI())));
+//            String invoicesJson = new String(Files.readAllBytes(Paths.get(getClass().getResource("currency.json").toURI())));
+//            System.out.println(invoicesJson);
+//            List<Currency> currencies = mapper.readValue(invoicesJson, new TypeReference<List<Currency>>() {});
+//            App.getDataStore().setAdditionalData(currencies);
+//            Dropdown dropdown = new Dropdown();
+//            dropdown.getItems().addAll(currencies.stream()
+//                    .map(Currency::getName)
+//                    .collect(Collectors.toList()));
+//            MainView.getInstance().getSettingsPage().getFormContainer().getChildren().addAll(currencyLabel, dropdown);
+        } catch (URISyntaxException e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    public static void main(String[] args) throws URISyntaxException {
+        CurrencyPlugin plug = new CurrencyPlugin();
+        ObjectMapper mapper = new ObjectMapper();
+        System.out.println(plug.getClass().getResource("/").getPath());
+
     }
 }
