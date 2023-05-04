f668a2fcabf673b3def6952db3d66e7052e59eac
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-05-04 15:47:04 +0700

    fix: update TransactionInvoiceCard

--- src/main/java/com/cbr/view/components/cards/TransactionInvoiceCard.java
+++ src/main/java/com/cbr/view/components/cards/TransactionInvoiceCard.java
@@ -2,6 +2,7 @@ package com.cbr.view.components.cards;
 
 import com.cbr.models.Product;
 import com.cbr.view.components.spinner.NumberSpinner;
+import com.cbr.view.pages.TransactionPage;
 import com.cbr.view.theme.Theme;
 import javafx.geometry.Insets;
 import javafx.geometry.Pos;
@@ -10,9 +11,17 @@ import javafx.scene.layout.HBox;
 import javafx.scene.layout.Priority;
 import javafx.scene.layout.Region;
 import javafx.scene.paint.Color;
+import lombok.Getter;
 
 public class TransactionInvoiceCard extends HBox {
-    public TransactionInvoiceCard(double width, double height, Product product) {
+    private TransactionPage parent;
+    @Getter
+    private Product product;
+    @Getter
+    private NumberSpinner numberSpinner;
+    public TransactionInvoiceCard(TransactionPage parent, double width, double height, Product product) {
+        this.parent = parent;
+        this.product = product;
         this.setMinWidth(width);
         this.setPrefWidth(width);
         this.setMaxWidth(width);
@@ -25,12 +34,13 @@ public class TransactionInvoiceCard extends HBox {
         Region spacer = new Region();
         HBox.setHgrow(spacer, Priority.ALWAYS);
 
-        Label pricePerItem = new Label("@" + String.format("%.0f", product.getSellPrice()));
+        Label pricePerItem = new Label("@" + String.format("%.2f", product.getSellPrice()));
         pricePerItem.setFont(Theme.getBodyMediumFont());
         pricePerItem.setTextFill(Color.WHITE);
         pricePerItem.setPadding(new Insets(0,20,0,20));
 
-        NumberSpinner numberSpinner = new NumberSpinner(1);
+        numberSpinner = new NumberSpinner(parent, 1);
+        numberSpinner.setProduct(product);
 
         this.getChildren().addAll(productName, spacer, pricePerItem, numberSpinner);
 
