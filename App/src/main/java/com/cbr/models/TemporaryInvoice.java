238f87cf197df31c15d347f44ad0d00fd3202d67
Jeffrey Chow <13521046@std.stei.itb.ac.id>
2023-05-05 16:09:34 +0700

    feat: no args constructor

--- App/src/main/java/com/cbr/models/TemporaryInvoice.java
+++ App/src/main/java/com/cbr/models/TemporaryInvoice.java
@@ -9,13 +9,15 @@ import java.time.LocalDateTime;
 import java.util.HashMap;
 import java.util.Map;
 
-@NoArgsConstructor
 @Getter
 @Setter
 public class TemporaryInvoice extends Invoice implements Serializable {
     protected Map<String, Integer> productFrequencies;  // <ProductId, Count>
     private static Integer invoiceCount = 0;
 
+    public TemporaryInvoice(){
+        TemporaryInvoice.invoiceCount += 1;
+    }
     public TemporaryInvoice(String customerId){
         super(customerId);
         this.productFrequencies = new HashMap<String, Integer>();
