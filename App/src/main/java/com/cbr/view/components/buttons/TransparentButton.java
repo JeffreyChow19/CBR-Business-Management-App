5c7cefb5e7aad44fe7ce7f121929888886d3ffab
yuujin-Q <13521074@mahasiswa.itb.ac.id>
2023-05-06 12:50:23 +0700

    chore: complete homepage styling

---
+++ App/src/main/java/com/cbr/view/components/buttons/TransparentButton.java
@@ -0,0 +1,20 @@
+package com.cbr.view.components.buttons;
+
+import com.cbr.view.theme.Theme;
+import javafx.scene.text.TextAlignment;
+
+public class TransparentButton extends DefaultButton {
+    public TransparentButton(String buttonName) {
+        super(Theme.getScreenWidth()*0.15, Theme.getScreenHeight()*0.10, buttonName);
+        this.setWrapText(true);
+        this.setTextAlignment(TextAlignment.CENTER);
+        this.setStyle("-fx-background-color: transparent;-fx-background-radius: 8; -fx-border-color: white; -fx-border-width: 2; -fx-border-radius: 8;");
+        this.setOnMouseEntered(event -> {
+            this.setStyle("-fx-background-color:"+ Theme.getSecondaryLight()+";-fx-background-radius: 8; -fx-border-color: white; -fx-border-width: 2; -fx-border-radius: 8;");
+        });
+        this.setOnMouseExited(event -> {
+            this.setStyle("-fx-background-color: transparent;-fx-background-radius: 8; -fx-border-color: white; -fx-border-width: 2; -fx-border-radius: 8;");
+        });
+
+    }
+}
--- App/src/main/java/com/cbr/view/pages/HomePage.java
+++ App/src/main/java/com/cbr/view/pages/HomePage.java
@@ -1,10 +1,13 @@
 package com.cbr.view.pages;
 
+import com.cbr.view.components.buttons.TransparentButton;
 import com.cbr.view.components.clockwidget.ClockWidget;
+import com.cbr.view.theme.Theme;
 import javafx.geometry.Pos;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
 import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
 
 public class HomePage extends StackPane {
     private Button clients;
@@ -15,9 +18,13 @@ public class HomePage extends StackPane {
 
     public HomePage() {
         super();
+        /* Theme */
+        this.setStyle("-fx-background-color:" + Theme.getPrimaryDark());
+        this.setMinSize(Theme.getScreenWidth(), Theme.getScreenHeight());
 
         /* First Layer Setup (Clock, NIM, Three Navigation Button)*/
         VBox firstLayer = new VBox();
+        firstLayer.setMinSize(Theme.getScreenWidth(), Theme.getScreenHeight());
         ClockWidget clockWidget = new ClockWidget();
         clockWidget.setAlignment(Pos.CENTER);
 
@@ -28,18 +35,23 @@ public class HomePage extends StackPane {
                 "13521094 - Angela Livia Arumsari\n" +
                 "13521100 - Alexander Jason");
         nim.setWrapText(true);
+        nim.setFont(Theme.getBodyFont());
+        nim.setTextFill(Color.WHITE);
 
         HBox navButtonsContainer = new HBox();
         navButtonsContainer.setSpacing(10.0);
         navButtonsContainer.setAlignment(Pos.CENTER);
-        clients = new Button("Clients");
-        transaction = new Button("Transactions");
-        inventoryManagement = new Button("Inventory Management");
+
+        clients = new TransparentButton("CLIENTS");
+        transaction = new TransparentButton("TRANSACTIONS");
+        inventoryManagement = new TransparentButton("INVENTORY MANAGEMENT");
         // chore: link buttons
         navButtonsContainer.getChildren().addAll(clients, transaction, inventoryManagement);
+        navButtonsContainer.setSpacing(Theme.getScreenWidth() * 0.04);
 
         firstLayer.setAlignment(Pos.CENTER);
         firstLayer.getChildren().addAll(clockWidget, nim, navButtonsContainer);
+        firstLayer.setSpacing(Theme.getScreenHeight() * 0.05);
 
 
         /* Second Layer Setup: "Capybucks", Settings and Plugins icons  */
