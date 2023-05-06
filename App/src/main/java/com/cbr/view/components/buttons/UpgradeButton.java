89a746a85f64f141fab92959444f8cec35aab8bd
yuujin-Q <13521074@mahasiswa.itb.ac.id>
2023-05-06 22:01:02 +0700

    chore: update UpgradeButton

--- App/src/main/java/com/cbr/view/components/buttons/UpgradeButton.java
+++ App/src/main/java/com/cbr/view/components/buttons/UpgradeButton.java
@@ -1,6 +1,7 @@
 package com.cbr.view.components.buttons;
 
 import com.cbr.view.components.header.tabmenu.TabMenuBar;
+import com.cbr.view.pages.ProfileEditor;
 import com.cbr.view.theme.Theme;
 import javafx.scene.control.Label;
 
@@ -9,7 +10,7 @@ public class UpgradeButton extends DefaultButton {
     public UpgradeButton(String customerId){
         super(Theme.getScreenWidth()*0.08, Theme.getScreenHeight()*0.05,"Upgrade");
         this.customerId = customerId;
-        this.setOnMouseClicked(event -> TabMenuBar.getInstance().addTab("Upgrade Customer#" + this.customerId, new Label("Upgrade Customer")));
+        this.setOnMouseClicked(event -> TabMenuBar.getInstance().addTab("Upgrade Customer#" + this.customerId, new ProfileEditor(this.customerId)));
         //chore: upgrade customer page
     }
 }
