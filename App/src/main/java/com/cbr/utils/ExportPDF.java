efddba899e6af8f97fa2894c51a7652651be972b
Angela Livia Arumsari <16521177@mahasiswa.itb.ac.id>
2023-05-07 22:45:02 +0700

    feat: init file chooser

--- App/src/main/java/com/cbr/utils/ExportPDF.java
+++ App/src/main/java/com/cbr/utils/ExportPDF.java
@@ -8,7 +8,6 @@ import java.util.List;
 import com.cbr.App;
 import com.cbr.models.BoughtProduct;
 import com.cbr.models.FixedInvoice;
-import com.cbr.models.InventoryProduct;
 import com.itextpdf.io.font.constants.StandardFonts;
 import com.itextpdf.kernel.colors.ColorConstants;
 import com.itextpdf.kernel.font.PdfFont;
@@ -23,33 +22,68 @@ import com.itextpdf.layout.element.Paragraph;
 import com.itextpdf.layout.element.Table;
 import com.itextpdf.layout.properties.HorizontalAlignment;
 import com.itextpdf.layout.properties.TextAlignment;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 import javafx.stage.FileChooser;
-import javafx.stage.Stage;
-
-import javax.swing.text.StyleConstants;
 
 public class ExportPDF
 {
     private String dest;
-//    public static void main(String[] args) {
-//        try {
-//            List<FixedInvoice> invoices = App.getDataStore().getInvoices().getDataList();
-//            ExportPDF("C:\\Users\\livia\\example.pdf", invoices.get(0));
-//        } catch (Exception e) {
-//            System.out.println("Error: " + e.getMessage());
-//        }
-//    }
-//    public void initStatement(Stage stage) {
-//        FileChooser fileChooser = new FileChooser();
-//        fileChooser.setTitle("Export Statement");
-//        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PDF Files", "*.pdf"));
-//        File selectedFile = fileChooser.showSaveDialog(stage);
-//
-//        if (selectedFile != null) {
-//            String filePath = selectedFile.getAbsolutePath();
-//            System.out.println(filePath);
-//        }
-//    }
+    public void init(Button buttonExport, FixedInvoice invoice) {
+        // create a new FileChooser dialog
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Export Invoice");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PDF Files", "*.pdf"));
+
+        // display the dialog and wait for user input
+        File selectedFile = fileChooser.showSaveDialog(buttonExport.getScene().getWindow());
+
+        // if user has chosen a file, get the file path
+        if (selectedFile != null) {
+            String filePath = selectedFile.getAbsolutePath();
+            dest = filePath;
+            try {
+                ExportPDF.exportPDF(dest, invoice);
+            } catch (Exception e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setContentText(e.getMessage());
+                alert.setOnCloseRequest(alertEvent -> {
+                    alert.close();
+                });
+                alert.show();
+            }
+        }
+    }
+
+    public void init(MenuItem menuExport) {
+        // create a new FileChooser dialog
+        FileChooser fileChooser = new FileChooser();
+        fileChooser.setTitle("Export Statement");
+        fileChooser.getExtensionFilters().add(new FileChooser.ExtensionFilter("PDF Files", "*.pdf"));
+
+        // display the dialog and wait for user input
+        File selectedFile = fileChooser.showSaveDialog(menuExport.getParentPopup().getScene().getWindow());
+
+        // if user has chosen a file, get the file path
+        if (selectedFile != null) {
+            String filePath = selectedFile.getAbsolutePath();
+            this.dest = filePath;
+            try {
+                List<FixedInvoice> invoices = App.getDataStore().getInvoices().getDataList();
+                ExportPDF.exportPDF(dest, invoices);
+            } catch (Exception e) {
+                Alert alert = new Alert(Alert.AlertType.ERROR);
+                alert.setTitle("Error");
+                alert.setContentText(e.getMessage());
+                alert.setOnCloseRequest(alertEvent -> {
+                    alert.close();
+                });
+                alert.show();
+            }
+        }
+    }
 
     public static void exportPDF(String dest, FixedInvoice invoice) throws Exception {
         PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
@@ -135,30 +169,6 @@ public class ExportPDF
         table.addCell(getPoint);
     }
     public static void exportPDF(String dest, List<FixedInvoice> invoices) throws Exception{
-        Map<LocalDate, List<FixedInvoice>> invoiceMap = new HashMap<>();
-        for (FixedInvoice invoice : invoices) {
-            LocalDate date = invoice.getCreatedAt().toLocalDate();
-            if (invoiceMap.containsKey(date)) {
-                invoiceMap.get(date).add(invoice);
-            } else {
-                List<FixedInvoice> invoiceList = new ArrayList<>();
-                invoiceList.add(invoice);
-                invoiceMap.put(date, invoiceList);
-            }
-        }
-
-        Map<LocalDate, Integer> rowSpan = new HashMap<>();
-        for (List<FixedInvoice> invoiceList : invoiceMap.values()) {
-            LocalDate date = invoiceList.get(0).getCreatedAt().toLocalDate();
-            int row = 0;
-            for (FixedInvoice invoice : invoiceList) {
-                row += 5;
-                row += invoice.getAdditionalCosts().size();
-                row += invoice.getBoughtProducts().size();
-            }
-            rowSpan.put(date, row + 2);
-        }
-
         PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
         Document doc = new Document(pdfDoc, PageSize.LETTER);
         doc.setMargins(24, 24, 24, 24);
@@ -169,40 +179,70 @@ public class ExportPDF
         PdfFont bold = PdfFontFactory.createFont(StandardFonts.HELVETICA_BOLD);
 
         // Title
-        Paragraph title = new Paragraph("Financial Statement").setFont(bold);
+        Paragraph title = new Paragraph("Financial Statement").setFont(bold).setTextAlignment(TextAlignment.CENTER);
         doc.add(title);
 
-        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy");
-        // Iterate date
-        for (Map.Entry<LocalDate, List<FixedInvoice>> entry : invoiceMap.entrySet()) {
-            LocalDate date = entry.getKey();
-            List<FixedInvoice> invoicesPerDate = entry.getValue();
-
-            Cell dateLabel = new Cell(rowSpan.get(date), 1).add(new Paragraph(date.format(formatter))).setFont(bold).setBorder(Border.NO_BORDER);
-            table.addCell(dateLabel);
-            Double income = 0.0;
-            Double revenue = 0.0;
-
-            for (FixedInvoice invoice : invoicesPerDate) {
-                Cell totalLabel = new Cell(1, 4).add(new Paragraph("Transaction " + invoice.getId())).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.BLUE).setFont(bold).setFontColor(ColorConstants.WHITE);
-                table.addCell(totalLabel);
-                income += invoice.getGrandTotal().getValue();
-                revenue += invoice.getRevenue();
-                ExportPDF.exportInvoice(invoice, table);
+        if (invoices.size() > 0) {
+            Map<LocalDate, List<FixedInvoice>> invoiceMap = new HashMap<>();
+            for (FixedInvoice invoice : invoices) {
+                LocalDate date = invoice.getCreatedAt().toLocalDate();
+                if (invoiceMap.containsKey(date)) {
+                    invoiceMap.get(date).add(invoice);
+                } else {
+                    List<FixedInvoice> invoiceList = new ArrayList<>();
+                    invoiceList.add(invoice);
+                    invoiceMap.put(date, invoiceList);
+                }
             }
 
-            // Total income and revenue
-            Cell totalLabel = new Cell(1, 3).add(new Paragraph("Total Income")).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.BLUE).setFont(bold).setFontColor(ColorConstants.WHITE);
-            table.addCell(totalLabel);
-            Cell total = new Cell().add(new Paragraph(income.toString())).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.BLUE).setFont(bold).setFontColor(ColorConstants.WHITE).setTextAlignment(TextAlignment.RIGHT);
-            table.addCell(total);
-            Cell getPointLabel = new Cell(1, 3).add(new Paragraph("Total Revenue")).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.BLUE).setFont(bold).setFontColor(ColorConstants.WHITE);
-            table.addCell(getPointLabel);
-            Cell getPoint = new Cell().add(new Paragraph(revenue.toString())).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.BLUE).setFont(bold).setFontColor(ColorConstants.WHITE).setTextAlignment(TextAlignment.RIGHT);
-            table.addCell(getPoint);
+            Map<LocalDate, Integer> rowSpan = new HashMap<>();
+            for (List<FixedInvoice> invoiceList : invoiceMap.values()) {
+                LocalDate date = invoiceList.get(0).getCreatedAt().toLocalDate();
+                int row = 0;
+                for (FixedInvoice invoice : invoiceList) {
+                    row += 5;
+                    row += invoice.getAdditionalCosts().size();
+                    row += invoice.getBoughtProducts().size();
+                }
+                rowSpan.put(date, row + 2);
+            }
+
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd MMM yyyy");
+            // Iterate date
+            for (Map.Entry<LocalDate, List<FixedInvoice>> entry : invoiceMap.entrySet()) {
+                LocalDate date = entry.getKey();
+                List<FixedInvoice> invoicesPerDate = entry.getValue();
+
+                Cell dateLabel = new Cell(rowSpan.get(date), 1).add(new Paragraph(date.format(formatter))).setFont(bold).setBorder(Border.NO_BORDER);
+                table.addCell(dateLabel);
+                Double income = 0.0;
+                Double revenue = 0.0;
+
+                for (FixedInvoice invoice : invoicesPerDate) {
+                    Cell totalLabel = new Cell(1, 4).add(new Paragraph("Transaction " + invoice.getId())).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.BLUE).setFont(bold).setFontColor(ColorConstants.WHITE);
+                    table.addCell(totalLabel);
+                    income += invoice.getGrandTotal().getValue();
+                    revenue += invoice.getRevenue();
+                    ExportPDF.exportInvoice(invoice, table);
+                }
+
+                // Total income and revenue
+                Cell totalLabel = new Cell(1, 3).add(new Paragraph("Total Income")).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.BLUE).setFont(bold).setFontColor(ColorConstants.WHITE);
+                table.addCell(totalLabel);
+                Cell total = new Cell().add(new Paragraph(income.toString())).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.BLUE).setFont(bold).setFontColor(ColorConstants.WHITE).setTextAlignment(TextAlignment.RIGHT);
+                table.addCell(total);
+                Cell getPointLabel = new Cell(1, 3).add(new Paragraph("Total Revenue")).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.BLUE).setFont(bold).setFontColor(ColorConstants.WHITE);
+                table.addCell(getPointLabel);
+                Cell getPoint = new Cell().add(new Paragraph(revenue.toString())).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.BLUE).setFont(bold).setFontColor(ColorConstants.WHITE).setTextAlignment(TextAlignment.RIGHT);
+                table.addCell(getPoint);
+            }
+            doc.add(table);
+        }
+        else {
+            Paragraph noTransaction = new Paragraph("You don't have any transaction yet.").setTextAlignment(TextAlignment.CENTER);
+            doc.add(noTransaction);
         }
 
-        doc.add(table);
         doc.close();
     }
 }
