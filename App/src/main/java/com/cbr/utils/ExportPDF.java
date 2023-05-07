082cdef3e9ff08c3f6b9c3d8e477e1ccfbf7bf38
Angela Livia Arumsari <16521177@mahasiswa.itb.ac.id>
2023-05-07 23:36:45 +0700

    fix: price format

--- App/src/main/java/com/cbr/utils/ExportPDF.java
+++ App/src/main/java/com/cbr/utils/ExportPDF.java
@@ -85,29 +85,6 @@ public class ExportPDF
         }
     }
 
-    public static void exportPDF(String dest, FixedInvoice invoice) throws Exception {
-        PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
-        Document doc = new Document(pdfDoc, PageSize.LETTER);
-        doc.setMargins(24, 24, 24, 24);
-
-        Table table = new Table(4).useAllAvailableWidth().setBorder(Border.NO_BORDER);
-        table.setHorizontalAlignment(HorizontalAlignment.CENTER);
-
-        PdfFont bold = PdfFontFactory.createFont(StandardFonts.HELVETICA_BOLD);
-
-        // Title
-        Paragraph title = new Paragraph("Invoice " + invoice.getId()).setFont(bold);
-        doc.add(title);
-
-        Paragraph customer = new Paragraph("Customer: " + invoice.getCustomerId());
-        doc.add(customer);
-
-        ExportPDF.exportInvoice(invoice, table);
-
-        doc.add(table);
-        doc.close();
-    }
-
     public static void exportInvoice(FixedInvoice invoice, Table table) throws Exception {
         PdfFont bold = PdfFontFactory.createFont(StandardFonts.HELVETICA_BOLD);
         // List of product
@@ -116,9 +93,8 @@ public class ExportPDF
         for (BoughtProduct product : productList) {
             Cell cell1 = new Cell().add(new Paragraph(product.getCount().toString())).setBorder(Border.NO_BORDER).setTextAlignment(TextAlignment.RIGHT);
             Cell cell2 = new Cell().add(new Paragraph(product.getProductName())).setBorder(Border.NO_BORDER);
-            Cell cell3 = new Cell().add(new Paragraph("@" +product.getSellPrice().getValue().toString())).setBorder(Border.NO_BORDER).setTextAlignment(TextAlignment.RIGHT);
-            Double totalPriceProduct = product.getSellPrice().getValue() * product.getCount();
-            Cell cell4 = new Cell().add(new Paragraph(totalPriceProduct.toString())).setBorder(Border.NO_BORDER).setTextAlignment(TextAlignment.RIGHT);
+            Cell cell3 = new Cell().add(new Paragraph("@" +product.getSellPrice().toString())).setBorder(Border.NO_BORDER).setTextAlignment(TextAlignment.RIGHT);
+            Cell cell4 = new Cell().add(new Paragraph(product.total().toString())).setBorder(Border.NO_BORDER).setTextAlignment(TextAlignment.RIGHT);
             if (isEvenRow) {
                 cell1.setBackgroundColor(ColorConstants.LIGHT_GRAY);
                 cell2.setBackgroundColor(ColorConstants.LIGHT_GRAY);
@@ -150,24 +126,49 @@ public class ExportPDF
         // Add discount and points used
         Cell discountLabel = new Cell(1, 3).add(new Paragraph("Discount")).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.LIGHT_GRAY);
         table.addCell(discountLabel);
-        Cell discount = new Cell().add(new Paragraph(invoice.getDiscount().getValue().toString())).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.LIGHT_GRAY).setHorizontalAlignment(HorizontalAlignment.RIGHT);
+        Cell discount = new Cell().add(new Paragraph(invoice.getDiscount().toString())).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.LIGHT_GRAY).setTextAlignment(TextAlignment.RIGHT);
         table.addCell(discount);
         Cell pointLabel = new Cell(1, 3).add(new Paragraph("Points Used")).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.LIGHT_GRAY);
         table.addCell(pointLabel);
-        Cell points = new Cell().add(new Paragraph(invoice.getUsedPoint().getValue().toString())).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.LIGHT_GRAY).setHorizontalAlignment(HorizontalAlignment.RIGHT);
+        Cell points = new Cell().add(new Paragraph(invoice.getUsedPoint().toString())).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.LIGHT_GRAY).setTextAlignment(TextAlignment.RIGHT);
         table.addCell(points);
 
 
         // Grand total and get points
         Cell totalLabel = new Cell(1, 3).add(new Paragraph("Grand Total")).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.LIGHT_GRAY).setFont(bold);
         table.addCell(totalLabel);
-        Cell total = new Cell().add(new Paragraph(invoice.getGrandTotal().getValue().toString())).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.LIGHT_GRAY).setTextAlignment(TextAlignment.RIGHT).setFont(bold);
+        Cell total = new Cell().add(new Paragraph(invoice.getGrandTotal().toString())).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.LIGHT_GRAY).setTextAlignment(TextAlignment.RIGHT).setFont(bold);
         table.addCell(total);
         Cell getPointLabel = new Cell(1, 3).add(new Paragraph("Points")).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.LIGHT_GRAY).setFont(bold);
         table.addCell(getPointLabel);
-        Cell getPoint = new Cell().add(new Paragraph(invoice.getGetPoint().getValue().toString())).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.LIGHT_GRAY).setTextAlignment(TextAlignment.RIGHT).setFont(bold);
+        Cell getPoint = new Cell().add(new Paragraph(invoice.getGetPoint().toString())).setBorder(Border.NO_BORDER).setBackgroundColor(ColorConstants.LIGHT_GRAY).setTextAlignment(TextAlignment.RIGHT).setFont(bold);
         table.addCell(getPoint);
     }
+
+    public static void exportPDF(String dest, FixedInvoice invoice) throws Exception {
+        PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
+        Document doc = new Document(pdfDoc, PageSize.LETTER);
+        doc.setMargins(24, 24, 24, 24);
+
+        Table table = new Table(4).useAllAvailableWidth().setBorder(Border.NO_BORDER);
+        table.setHorizontalAlignment(HorizontalAlignment.CENTER);
+
+        PdfFont bold = PdfFontFactory.createFont(StandardFonts.HELVETICA_BOLD);
+
+        // Title
+        Paragraph title = new Paragraph("Invoice " + invoice.getId()).setFont(bold);
+        doc.add(title);
+
+        Paragraph customer = new Paragraph("Customer: " + invoice.getCustomerId());
+        doc.add(customer);
+
+        ExportPDF.exportInvoice(invoice, table);
+
+        doc.add(table);
+        doc.close();
+    }
+
+
     public static void exportPDF(String dest, List<FixedInvoice> invoices) throws Exception{
         PdfDocument pdfDoc = new PdfDocument(new PdfWriter(dest));
         Document doc = new Document(pdfDoc, PageSize.LETTER);
